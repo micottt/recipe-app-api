d01155e4ca9e12c6a231163899c375ac4ff8e61a
Xicheng Tang <xichengtang@Xichengs-MacBook-Pro.local>
2024-03-07 23:23:14 -0500

    Condigured docker compose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,8 +2,10 @@
 Calculator Functions
 """
 
+
 def add(x, y):
     return x + y
+
 
 def subtract(x, y):
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -4,6 +4,7 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
